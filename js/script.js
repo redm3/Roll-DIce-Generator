f51fe9685cc647cdac48ad45e86d1e059b699804
marco <macmwz20@gmail.com>
2023-02-16 00:38:10 +1300

    yellow1

--- js/script.js
+++ js/script.js
@@ -1,27 +1,5 @@
 function rollDice() {
 
-/*     <img style="display:none;" id="10pic" src="https://media.tenor.com/flDyUOp9NDoAAAAi/d20-dice-roll.gif" />
-    <img style="display:none;" id="10-1pic" src="https://media.tenor.com/tCq5mfycACMAAAAi/d20-dice-roll.gif" />
-    <img style="display:none;" id="10-2pic" src="https://media.tenor.com/zTg9FhOqpDIAAAAi/d20-dice-roll.gif" />
-    <img style="display:none;" id="10-3pic" src="https://media.tenor.com/8UcqJlc93EgAAAAi/d20-dice-roll.gif" />
-    <img style="display:none;" id="10-4pic" src="https://media.tenor.com/l-95tTbEKGkAAAAi/d20-dice-roll.gif" />
-    <img style="display:none;" id="10-5pic" src="https://media.tenor.com/r7bAmy9lpJUAAAAi/d20-dice-roll.gif" />
-    <img style="display:none;" id="10-6pic" src="https://media.tenor.com/HEHpab5WZHwAAAAi/d20-dice-roll.gif" />
-    <img style="display:none;" id="10-7pic" src="https://media.tenor.com/jTOrIZkFmVYAAAAi/d20-dice-roll.gif" />
-    <img style="display:none;" id="10-8pic" src="https://media.tenor.com/FFLUTiVgFNsAAAAi/d20-dice-roll.gif" />
-    <img style="display:none;" id="10-9pic" src="https://media.tenor.com/92v1rySX6QUAAAAi/d20-dice-roll.gif" />
-    <img style="display:none;" id="10-10pic" src="https://media.tenor.com/flDyUOp9NDoAAAAi/d20-dice-roll.gif" />
-
- */
-
-/*     <img style="display:none;" id="6-1pic" src="https://media.tenor.com/BZGKCKH8Wp4AAAAi/dice-roll-dice.gif" />
-    <img style="display:none;" id="6-2pic" src="https://media.tenor.com/VB6lPcSFiVgAAAAi/dice2-dice.gif" />
-    <img style="display:none;" id="6-3pic" src="https://media.tenor.com/Pq2avhc9XvkAAAAi/dice-roll-dice.gif" />
-    <img style="display:none;" id="6-4pic" src="https://media.tenor.com/cHiHze95e3cAAAAj/dice-roll-dice.gif" />
-    <img style="display:none;" id="6-5pic" src="https://media.tenor.com/iBb9CXPm3icAAAAi/dice-roll-dice.gif" />
-    <img style="display:none;" id="6-6pic" src="https://media.tenor.com/HcK7RSiai-AAAAAj/dice-roll-dice.gif" /> */
-
-
     var diceType = document.getElementById("dice-type").value;
     
     if (diceType == 6) {    
@@ -29,6 +7,7 @@ function rollDice() {
         var img = document.getElementById('6pic')
         img.src = sourceOfPicture.replace('90x90', '225x225');
         img.style.display = "block";
+        //setTimeout(function() {img.style.display = 'none';}, 3500);
         //setTimeout(function() {img.style.display = 'none';}, 2000); // 1000 milliseconds = 1 seconds
     }
     
@@ -55,7 +34,7 @@ function rollDice() {
         if(result==2){
             var sourceOfPicture = "https://media.tenor.com/VB6lPcSFiVgAAAAj/dice2-dice.gif";
             var img = document.getElementById('6-2pic')
-            img.src = sourceOfPicture.replace('20x20', '100x100');
+            img.src = sourceOfPicture.replace('90x90', '225x225');
             img.style.display = "block";
             setTimeout(function() {img.style.display = 'none';}, 3500); // 1000 milliseconds = 1 seconds
         }
