428c8786335973d52797eaaf6eaf4e7e4990108e
marco <macmwz20@gmail.com>
2023-02-16 11:24:47 +1300

    testing

--- js/script.js
+++ js/script.js
@@ -1,14 +1,20 @@
 function rollDice() {
     var diceType = document.getElementById("dice-type").value;
-    var result = Math.floor(Math.random() * diceType) + 1;
+    var result = testRandomNumber(diceType);
     document.getElementById("result").innerHTML = "Result: " + result;
   }
 
-  rollDice();
+
+
+  function testRandomNumber(diceType){
+    var result = Math.floor(Math.random() * diceType) + 1;
+    return result;
+  }
+
 
   //test if function returns a number 
   function testRollDiceReturnsNumber() {
-    const result = rollDice();
+    const result = testRandomNumber(6);
     if (typeof result === 'number') {
       console.log('Test 1 passed!');
     } else {
