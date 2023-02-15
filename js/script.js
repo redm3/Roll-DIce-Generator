5c203371b53b20af86e24b7cb9e7f68806e3d527
marco <macmwz20@gmail.com>
2023-02-15 18:45:15 +1300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -3,3 +3,15 @@ function rollDice() {
     var result = Math.floor(Math.random() * diceType) + 1;
     document.getElementById("result").innerHTML = "Result: " + result;
   }
+
+  //test if function returns a number 
+  function testRollDiceReturnsNumber() {
+    const result = rollDice();
+    if (typeof result === 'number') {
+      console.log('Test 1 passed!');
+    } else {
+      console.error('Test 1 failed.');
+    }
+  }
+
+  testRollDiceReturnsNumber();
